adf4cb02b71dba09a42d810d0afbb0f5c6894a49
LeTeeno <105130204+LeTeeno@users.noreply.github.com>
2024-12-06 04:32:29 +0100

    Update pay.js

--- src/commands/pay.js
+++ src/commands/pay.js
@@ -4,7 +4,7 @@ const { createCanvas, loadImage, registerFont } = require("canvas");
 const path = require("path");
 
 const uri =
-  "mongodb+srv://admin:SixrYaxUeu8KwDhA@cluster0.rayv48e.mongodb.net/?retryWrites=true&w=majority";
+  "REPLACE_WITH_YOUR_URI";
 
 // Define the user schema
 const userSchema = new mongoose.Schema({
