7ea7c27eaf190e4e80aa6a097b7580c9fbfd3a2a
LeTeeno <105130204+LeTeeno@users.noreply.github.com>
2024-12-06 04:34:17 +0100

    Update redeem.js

--- src/commands/redeem.js
+++ src/commands/redeem.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
 
 // Connect to MongoDB
 mongoose.connect(
-  "mongodb+srv://admin:SixrYaxUeu8KwDhA@cluster0.rayv48e.mongodb.net/?retryWrites=true&w=majority",
+  "REPLACE_WITH_YOUR_URI",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
